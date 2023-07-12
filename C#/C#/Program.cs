3a12d3b7af81204cdc0b701d4b73c0f5f17547be
Gahramanov Samir <samirkhg@code.local>
2023-07-12 18:14:29 +0400

    add new folder

---
+++ C#/C#/Program.cs
@@ -0,0 +1,90 @@
+﻿//             1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın
+
+//int n = 21;
+
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("Bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+//2)n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+//int n = 4;
+//int m = 6;
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//    Console.WriteLine(n + m);
+//}
+
+
+//3) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+
+
+// int n = 3;
+//int m = 29;
+//int sum = 0;
+
+//for (int i = n; i < m; i++)
+//{
+//    if (i % 2 != 0)
+//    {
+//        sum += i;
+
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+
+//// 4) Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+
+
+//int sum = 0;
+
+//int[] nums = { 1, 6, 253, 54, 2, 99 };
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+
+
+//    if (nums[i] % 2 != 0)
+//    {
+
+//        sum += nums[i];
+
+//    }
+
+
+//}
+//Console.WriteLine(sum);
+
+//5) Verilmish arrayin icindeki cut ededlerin sayini tapin
+
+
+
+//int count = 0;
+
+//int[] nums = { 1, 6, 253, 54, 2, 99 };
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+
+//    if (nums[i] % 2 == 0)
+//    {
+
+//        count += nums[i];
+
+//     }
+
+
+//}
+
+//Console.WriteLine(count);
